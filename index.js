7c2b82482449ca1d87ea69065137e962e193b2c8
121ananya <65679502+121ananya@users.noreply.github.com>
2023-02-26 23:03:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const express = require('express');
 var app = express();
 const bodyparser = require('body-parser');
@@ -8,9 +8,8 @@ app.use(bodyparser.json());
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
-    database: 'EmployeeDB',
-    multipleStatements: true
+    password: 'Y1012Jqkhkp',
+    database: 'contact_crm_db'
 });
 
 mysqlConnection.connect((err) => {
@@ -24,9 +23,21 @@ mysqlConnection.connect((err) => {
 app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
 
 
-//Get all employees
-app.get('/employees', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Employee', (err, rows, fields) => {
+//Get all contacts
+app.get('/contacts', (req, res) => {
+    mysqlConnection.query('SELECT * FROM ContactsTB1', (err, rows) => {
+    console.log('rows :', rows);
+        if (!err){
+            res.send(rows);
+        }
+        else
+            console.log(err);
+    })
+});
+
+//Get an contacts
+app.get('/contacts/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM ContactsTB1 WHERE id = ?', [req.params.id], (err, rows, fields) => {
         if (!err)
             res.send(rows);
         else
@@ -34,19 +45,9 @@ app.get('/employees', (req, res) => {
     })
 });
 
-//Get an employees
-app.get('/employees/:id', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
-            res.send(rows);
-        else
-            console.log(err);
-    })
-});
-
-//Delete an employees
-app.delete('/employees/:id', (req, res) => {
-    mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
+//Delete an contacts
+app.delete('/contacts/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM ContactsTB1 WHERE id = ?', [req.params.id], (err, rows, fields) => {
         if (!err)
             res.send('Deleted successfully.');
         else
@@ -54,28 +55,28 @@ app.delete('/employees/:id', (req, res) => {
     })
 });
 
-//Insert an employees
-app.post('/employees', (req, res) => {
-    let emp = req.body;
-    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+//Insert an contacts
+app.post('/contacts', (req, res) => {
+    let con = req.body;
+    var sql = "SET @id = ?;SET @first_name = ?;SET @last_name = ?;SET @email = ?;SET @mobile_number = ?\
+    CALL ContactAddOrEdit(@id,@first_name,@last_name,@email,@mobile_number);";
+    mysqlConnection.query(sql, [con.id, con.first_name, con.last_name, con.email ,con.mobile_number], (err, rows, fields) => {
         if (!err)
             rows.forEach(element => {
                 if(element.constructor == Array)
-                res.send('Inserted employee id : '+element[0].EmpID);
+                res.send('Inserted contact id : '+element[0].id);
             });
         else
             console.log(err);
     })
 });
 
-//Update an employees
-app.put('/employees', (req, res) => {
-    let emp = req.body;
-    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+//Update an contacts
+app.put('/contacts', (req, res) => {
+    let con = req.body;
+    var sql = "SET @id = ?;SET @first_name = ?;SET @last_name = ?;SET @email = ?;SET @mobile_number = ?\
+    CALL ContactAddOrEdit(@id,@first_name,@last_name,@email,@mobile_number);";
+    mysqlConnection.query(sql, [con.id, con.first_name, con.last_name, con.email ,con.mobile_number], (err, rows, fields) => {
         if (!err)
             res.send('Updated successfully');
         else
